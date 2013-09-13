96fb9eef6278b48f871ea800e178eb62d8080027
reed-kochel <rkochel.kochel@gmail.com>
2013-09-12 20:08:18 -0600

    Kochel_Reed_Expressions_Industry
    Class Assignment

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// JavaScript
+
+//this changes the css of your body tag to be green
+document.querySelector("body").style.backgroundColor= "#00FF00";
+
+//kochel_Reed_Expressions_Industry
+
+
+
+//Determining how many pixels per inch are in a tv screen
+
+var height = 1080; //size of screen going diagonal
+var width = 1920;  //size of screen going horizontal
+var pixels = width * height; //the formula to determine pixel count
+console.log("This is how many pixels are in my TV"); //console message to show MY result
+console.log(pixels);
+var userInput = prompt("Enter screen height"); //having user put info into prompt
+var userInput = prompt("Enter screen width"); //having user put info into prompt
+console.log("This is how many pixels are in your tv"); //console message directed at user
+alert("This is how many pixels are in your TV:");
+console.log(pixels);
+
